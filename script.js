d4a3c905e2a20562c15678d3d7e78b05efa19060
sushil0c <139460233+sushil0c@users.noreply.github.com>
2025-03-08 01:11:20 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // Fetch radio stations from the URL
     const fetchRadioStations = async () => {
         try {
-            const response = await fetch('https://raw.githubusercontent.com/sushil0c/novels/main/radio.json');
+            const response = await fetch('https://raw.githubusercontent.com/2shrestha22/radio/main/assets/radio_list.json
+');
             stationsList = await response.json();
             renderRadioStations(stationsList);
         } catch (error) {
